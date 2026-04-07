3885bc48aa7402fbc56362e2469881003d796e3c
lakshmiakakshatangella2-lab <lakshmiakakshatangella2@gmail.com>
2026-04-07 12:05:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,9 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
     from flask import Flask
 
-from flask import render_template
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
