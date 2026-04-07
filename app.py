8ad0e837b1be6910106251845c5beed239c94d50
lakshmiakakshatangella2-lab <lakshmiakakshatangella2@gmail.com>
2026-04-07 10:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -253,11 +253,6 @@ if __name__ == '__main__':
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "My app is running"
-
-import os
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
