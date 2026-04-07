ea5f4fab06f6a574770e003d78b7a45f8acebc9a
lakshmiakakshatangella2-lab <lakshmiakakshatangella2@gmail.com>
2026-04-07 11:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -251,8 +251,8 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
     from flask import Flask
 
-app = Flask(__name__)
+from flask import render_template
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route("/")
+def home():
+    return render_template("index.html")
